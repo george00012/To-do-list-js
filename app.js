86b4cb4a4110e47d570efe72242e5a0f80d381cb
george00012 <georgenarcis12@yahoo.es>
2023-04-09 23:45:18 +0200

    actualizacion 2

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
 const enviar = document.querySelector(".contenido-enviar");
 const tarea = document.querySelector(".contenido-tarea");
-const contador = document.querySelector(".contenido-contador");
+const contadorTarea = document.querySelector(".contenido-contador");
 const borrarTodo = document.querySelector(".contenido-eliminar");
+const lista = document.querySelector(".contenido-lista-tareas");
+let contar = 0;
 
-function agregarTareas(){
+function agregarTareas() {
+    /*tarea*/
     const escribirTarea = tarea.value;
-
     const nuevaLista = document.createElement("li");
-    nuevaLista.textContent = escribirTarea;
+    const eliminarBtn = document.createElement("button");
+    eliminarBtn.textContent = "";
+    eliminarBtn.classList.add("eliminarLista");
+    eliminarBtn.addEventListener("click", () => {
+        lista.removeChild(nuevaLista);
+        contar--;
+        contadorTarea.textContent = `Hay ${contar} tarea${plural(contar)}`;
+    });
 
-    const lista = document.querySelector(".lista-tareas");
+    nuevaLista.textContent = escribirTarea + " ";
+    nuevaLista.appendChild(eliminarBtn);
     lista.appendChild(nuevaLista);
+    tarea.value = "";
 
-    tarea.value ="";
+    /*contador*/
+    contar++;
+    contadorTarea.textContent = `Hay ${contar} tarea${plural(contar)}`;
+
+    /*editar*/
+    nuevaLista.addEventListener("click", () =>{
+        nuevaLista.contentEditable = true;
+        nuevaLista.focus();
+    });
+
+    /*eliminar todo*/
+    borrarTodo.addEventListener("click", () => {
+        lista.innerHTML = "";
+        contadorTarea.innerHTML = `Hay 0 tareas`;
+        contar = 0;
+    });
+
 }
+
 enviar.addEventListener("click", agregarTareas);
-tarea.addEventListener("keydown", function(event) {
-    if(event.keyCode === 13){
+tarea.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
         event.preventDefault();
         agregarTareas();
     }
 });
 
+function plural(cantidad) {
+    if (cantidad != 1) {
+        return "s";
+    } else {
+        return ""
+    }
+}
