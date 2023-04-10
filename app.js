3fbd22d901675570f37c9b5b37286e14e1086483
george00012 <georgenarcis12@yahoo.es>
2023-04-10 13:48:28 +0200

    actualizacion 3

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const borrarTodo = document.querySelector(".contenido-eliminar");
 const lista = document.querySelector(".contenido-lista-tareas");
 let contar = 0;
 
+
 function agregarTareas() {
     /*tarea*/
     const escribirTarea = tarea.value;
     const nuevaLista = document.createElement("li");
+    nuevaLista.classList.add("listaAñadida");
     const eliminarBtn = document.createElement("button");
     eliminarBtn.textContent = "";
     eliminarBtn.classList.add("eliminarLista");
@@ -16,6 +18,9 @@ function agregarTareas() {
         lista.removeChild(nuevaLista);
         contar--;
         contadorTarea.textContent = `Hay ${contar} tarea${plural(contar)}`;
+        if (contar == 0) {
+            contadorTarea.textContent = `No tienes tareas`;
+        }
     });
 
     nuevaLista.textContent = escribirTarea + " ";
@@ -27,19 +32,12 @@ function agregarTareas() {
     contar++;
     contadorTarea.textContent = `Hay ${contar} tarea${plural(contar)}`;
 
-    /*editar*/
-    nuevaLista.addEventListener("click", () =>{
-        nuevaLista.contentEditable = true;
-        nuevaLista.focus();
-    });
-
     /*eliminar todo*/
     borrarTodo.addEventListener("click", () => {
         lista.innerHTML = "";
-        contadorTarea.innerHTML = `Hay 0 tareas`;
+        contadorTarea.innerHTML = `No tienes tareas`;
         contar = 0;
     });
-
 }
 
 enviar.addEventListener("click", agregarTareas);
